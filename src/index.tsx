5d5cdceb0b66d09f878235886fc516f332f77d3b
Michal Bláha <michal.blaha@timeisltd.com>
2020-04-09 09:41:35 +0200

    Add Sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/browser';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+Sentry.init({ dsn: 'https://62db57ae119448df80034147629d4a0c@o148599.ingest.sentry.io/5194594' });
 
 ReactDOM.render(
   <React.StrictMode>
